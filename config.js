686332e55835ec59e699bf089e2cce82390f3ef4
Viet Hoang <dvhoang2106@gmail.com>
2021-11-16 14:16:04 +0700

    add

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu cậu! Tớ là Diệu Phương xinh đẹp',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'okeeei',
+    title: 'Cậu yêu tớ có phải không 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là ny tớ đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'cậu mơ à :333',
+    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì bạn khùng nhưng vì yêu bạn nên mình yêu mọi thứ về bạn:>>>>',
+    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/dieu.phuong.1808' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
